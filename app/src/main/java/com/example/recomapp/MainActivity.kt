79952b46f587b218d11e000b917d20886d1cd686
Andrew Fei <afei3@illinois.edu>
2020-02-15 17:56:16 -0600

    Redid second activity to fix crashing

--- app/src/main/java/com/example/recomapp/MainActivity.kt
+++ app/src/main/java/com/example/recomapp/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.recomapp
 
 import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
@@ -16,8 +15,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
         fab.setOnClickListener {
-            //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-            //    .setAction("Action", null).show()
             val intent = Intent(this, SearchResult::class.java)
             startActivity(intent)
         }
--- app/src/main/java/com/example/recomapp/SearchResult.kt
+++ app/src/main/java/com/example/recomapp/SearchResult.kt
@@ -1,6 +1,5 @@
 package com.example.recomapp
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
@@ -11,19 +10,5 @@ class SearchResult : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.searchresult)
-        fab2.setOnClickListener {
-            Toast.makeText(applicationContext,"this is toast message",Toast.LENGTH_SHORT).show()
-        }
-    }
-
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 }
