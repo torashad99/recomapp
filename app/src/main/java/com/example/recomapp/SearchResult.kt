ba06d346186b5b370fbd146c22db2779c8cf19cf
Andrew Fei <afei3@illinois.edu>
2020-02-16 00:56:55 -0600

    Replaced snom with template

--- app/src/main/java/com/example/recomapp/SearchResult.kt
+++ app/src/main/java/com/example/recomapp/SearchResult.kt
@@ -1,10 +1,7 @@
 package com.example.recomapp
 
 import android.os.Bundle
-import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.searchresult.*
 
 class SearchResult : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
